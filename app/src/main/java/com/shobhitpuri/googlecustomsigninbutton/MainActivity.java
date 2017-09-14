31b620a8ae90cae6c97efe95d06b69362c6419d9
Shobhit Puri <shobhit.iiita@gmail.com>
2017-09-13 17:54:09 -0700

    Added new customized google signin button that follows Google guidelines, where we can use android:setText easily

---
+++ app/src/main/java/com/shobhitpuri/googlecustomsigninbutton/MainActivity.java
@@ -0,0 +1,16 @@
+package com.shobhitpuri.googlecustomsigninbutton;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Sample activity to show the Custom Google Sign in button
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
