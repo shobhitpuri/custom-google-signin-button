26774fccfe3e96920a15a9a102587b20f5925721
hanhan <matcherapy@gmail.com>
2019-10-10 10:22:16 +0700

    migrate to AndroidX

--- app/src/main/java/com/shobhitpuri/googlecustomsigninbutton/MainActivity.java
+++ app/src/main/java/com/shobhitpuri/googlecustomsigninbutton/MainActivity.java
@@ -1,7 +1,7 @@
 package com.shobhitpuri.googlecustomsigninbutton;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * Sample activity to show the Custom Google Sign in button
